81a8b2ca1969e1b3523e6e1a7382d31fa96bcae4
LightCakeBY <s.pirahova@gmail.com>
2021-07-21 11:32:27 +0300

    first commit

---
+++ webapp/Component.js
@@ -0,0 +1,7 @@
+sap.ui.define(['sap/suite/ui/generic/template/lib/AppComponent'], function(AppComponent) {
+    return AppComponent.extend("testfiori.Component", {
+        metadata: {
+            manifest: "json"
+        }
+    });
+});
