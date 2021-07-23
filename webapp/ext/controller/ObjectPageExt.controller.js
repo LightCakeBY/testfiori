4583ec3ec2626d3e8951292d3464336152161c4f
LightCakeBY <s.pirahova@gmail.com>
2021-07-23 15:14:00 +0300

    Step 8. Title, Desc and Status on ObjPage

--- webapp/ext/controller/ObjectPageExt.controller.js
+++ webapp/ext/controller/ObjectPageExt.controller.js
@@ -5,7 +5,7 @@ sap.ui.controller("testfiori.ext.controller.ObjectPageExt", {
     },
     onInit: function (){
         this.extensionAPI.attachPageDataLoaded(function(oEvent){
-			sap.m.MessageToast.show("Current Category ID: " + oEvent.context.getProperty("CategoryID"));
+			sap.m.MessageToast.show("Current Category ID is " + oEvent.context.getProperty("CategoryID") + ": " +oEvent.context.getProperty("CategoryName"));
 		});
     }
 });
