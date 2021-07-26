e51566ec28fb0f07420e43e4a1adc127d558ffee
LightCakeBY <s.pirahova@gmail.com>
2021-07-26 14:32:41 +0300

    Step 12. Actions

--- webapp/ext/controller/ObjectPageExt.controller.js
+++ webapp/ext/controller/ObjectPageExt.controller.js
@@ -1,11 +1,23 @@
+var categoryID;
 sap.ui.controller("testfiori.ext.controller.ObjectPageExt", {
     
-    onCustomAction : function(oEvent) { 
-        alert('onCustomAction');
+    onDeleteAction : function(oEvent) { 
+        sap.m.MessageToast.show("Hello world!")
     },
+
     onInit: function (){
         this.extensionAPI.attachPageDataLoaded(function(oEvent){
 			sap.m.MessageToast.show("Current Category ID is " + oEvent.context.getProperty("CategoryID") + ": " +oEvent.context.getProperty("CategoryName"));
-		});
+            categoryID = oEvent.context.getProperty("CategoryID");
+        });
+    },
+
+    toActiveStatus: function(oEvent){
+        
+            this.getOwnerComponent().getModel().setProperty("/Categories(" + categoryID + ")/StatusID", 2);
+            this.getOwnerComponent().getModel().submitChanges();
+            this.getOwnerComponent().getModel().refresh();
+	
+        sap.m.MessageToast.show("Category was activated!")
     }
 });
