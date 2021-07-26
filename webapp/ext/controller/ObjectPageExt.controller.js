931172826d29ebf10cccda54c7b57a028daf1fc6
LightCakeBY <s.pirahova@gmail.com>
2021-07-26 15:43:54 +0300

    Step 13. Mock server extendend

--- webapp/ext/controller/ObjectPageExt.controller.js
+++ webapp/ext/controller/ObjectPageExt.controller.js
@@ -12,12 +12,34 @@ sap.ui.controller("testfiori.ext.controller.ObjectPageExt", {
         });
     },
 
-    toActiveStatus: function(oEvent){
+    toActiveStatus: function(){
         
             this.getOwnerComponent().getModel().setProperty("/Categories(" + categoryID + ")/StatusID", 2);
             this.getOwnerComponent().getModel().submitChanges();
             this.getOwnerComponent().getModel().refresh();
 	
         sap.m.MessageToast.show("Category was activated!")
-    }
+    },
+
+    onRandom: function(){
+        
+        this.getOwnerComponent().getModel().callFunction('/onRandom',{
+			method: "GET",
+			success: function() {
+				var iPreviousStatus = this.getOwnerComponent().getModel().getProperty("/Categories(" + categoryID + ")/StatusID");
+
+				if(iPreviousStatus==2){
+					sap.m.MessageToast.show("Category is already active");	
+				}else {
+					this.getOwnerComponent().getModel().setProperty("/Categories(" + categoryID + ")/StatusID", 2);
+					this.getOwnerComponent().getModel().submitChanges();
+					this.getOwnerComponent().getModel().refresh();
+				}
+			}.bind(this),
+			error: function() {
+				sap.m.MessageToast.show("Error status!!!");
+			}
+		});     
+}
 });
+//<FunctionImport Name="toActiveStatus" EntitySet="Categories" m:HttpMethod="GET"></FunctionImport>
